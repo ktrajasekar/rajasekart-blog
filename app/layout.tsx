f35e955d99149c5ce80ba808260eab7192ad8685
Rajasekar Thangavel <kt.rajasekar@gmail.com>
2024-03-03 21:51:40 +0530

    Minor fix in header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "./Components/header";
+import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
